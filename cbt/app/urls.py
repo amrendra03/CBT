a5a8f3277e4579b738ac5f5c3ee1f73057a1e70c
Amrendrarao <93829160+Amrendrarao@users.noreply.github.com>
2023-10-02 15:17:07 +0530

    all routes setup completed

--- cbt/app/urls.py
+++ cbt/app/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("login/", views.login, name="login"),
     path("forgetpassword/", views.forgetPassword, name="forgetpassword"),
-    path("singup/", views.singup, name="singup"),
+    path("signup/", views.signup, name="signup"),
+    path("profile/", views.profile, name="profile"),
+    path("exam/", views.exam, name="exam"),
+    path("candidate/", views.candidate, name="candidate"),
+    path("question/", views.question, name="question"),
+    path("result/", views.result, name="result"),
+    path("setting/", views.setting, name="setting"),
+    path("about/", views.about, name="about"),
 ]
--- cbt/app/views.py
+++ cbt/app/views.py
@@ -12,9 +12,41 @@ def login(req):
     return render(req, "login.html")
 
 
-def singup(req):
-    return render(req, "singup.html")
+def signup(req):
+    return render(req, "signup.html")
 
 
 def forgetPassword(req):
     return render(req, "forgetPassword.html")
+
+
+def index(req):
+    return render(req, "index.html")
+
+
+def profile(req):
+    return render(req, "profile.html")
+
+
+def exam(req):
+    return render(req, "exam.html")
+
+
+def candidate(req):
+    return render(req, "candidate.html")
+
+
+def question(req):
+    return render(req, "question.html")
+
+
+def result(req):
+    return render(req, "result.html")
+
+
+def setting(req):
+    return render(req, "setting.html")
+
+
+def about(req):
+    return render(req, "about.html")
